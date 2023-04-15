937275a1fae37e7a6df8e22cd445df4725938c53
osm1892 <osm189214@gmail.com>
2023-04-16 01:59:34 +0900

    fix : 중대한 버그 수정

--- opencv/opencv.py
+++ opencv/opencv.py
@@ -7,8 +7,12 @@ FLUENT_BIT_HOST = "localhost"  # Fluent Bit 서버의 IP 주소 또는 호스트
 FLUENT_BIT_PORT = 30000  # Fluent Bit 서버의 포트
 
 sender = asyncsender.FluentSender(
-    "opencv", host=FLUENT_BIT_HOST, port=FLUENT_BIT_PORT, nanosecond_precision=True, queue_maxsize=1000,
-    queue_circular=True
+    "opencv",
+    host=FLUENT_BIT_HOST,
+    port=FLUENT_BIT_PORT,
+    nanosecond_precision=True,
+    queue_maxsize=1000,
+    queue_circular=True,
 )
 
 cap = cv2.VideoCapture(0)
@@ -23,5 +27,5 @@ while True:
     if not ret:
         break
 
-    data = cv2.imencode(".webp", frame, [cv2.IMWRITE_WEBP_QUALITY, 100]).tostring()
+    data = cv2.imencode(".webp", frame, [cv2.IMWRITE_WEBP_QUALITY, 100])[1].tolist()
     sender.emit("data", {"frame": data})
