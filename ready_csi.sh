d7ab79bb41c27bc8f88d10ec94acfe5d1a1b882f
osm1892 <osm189214@gmail.com>
2023-04-16 04:09:30 +0900

    fix : 의도대로 작동하지 않는 코드 삭제

--- ready_csi.sh
+++ ready_csi.sh
@@ -5,7 +5,6 @@ sudo mount -t tmpfs -o size=50M tmpfs tmp
 
 TARGET_INTERFACE="wlan0"
 MONITOR_INTERFACE="mon0"
-SECOND_WLAN="wlx909f33eea782"
 MCP_CORE_MASK=1
 MCP_STREAM_MASK=1
 MCP_CHANNEL_BANDWIDTH="8/20"
@@ -20,8 +19,3 @@ sudo ifconfig $TARGET_INTERFACE up
 sudo nexutil -I$TARGET_INTERFACE -s500 -b -l34 -v$MCP
 sudo iw dev $TARGET_INTERFACE interface add mon0 type monitor
 sudo ip link set $MONITOR_INTERFACE up
-
-if ifconfig "$SECOND_WLAN" >/dev/null 2>&1; then
-  sudo systemctl start iwd
-  iwctl -passphrase "$PASSPHRASE" station $SECOND_WLAN connect "$SSID"
-fi
