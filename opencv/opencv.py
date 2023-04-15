67aba906cfe644659106d4f25d39569fcbc2ce72
osm1892 <osm189214@gmail.com>
2023-04-16 00:30:57 +0900

    feat : 데이터 수집 변경
    기존 sync 기반 sending을 async로 변경하여 데이터 수집 간격이 벌어지지 않도록 변경.
    opencv 데이터 수집 코드 추가를 통한 학습 라벨링용 이미지 수집 파이프라인 구축

---
+++ opencv/opencv.py
@@ -0,0 +1,27 @@
+import sys
+
+import cv2
+from fluent import asyncsender
+
+FLUENT_BIT_HOST = "localhost"  # Fluent Bit 서버의 IP 주소 또는 호스트명
+FLUENT_BIT_PORT = 30000  # Fluent Bit 서버의 포트
+
+sender = asyncsender.FluentSender(
+    "opencv", host=FLUENT_BIT_HOST, port=FLUENT_BIT_PORT, nanosecond_precision=True, queue_maxsize=1000,
+    queue_circular=True
+)
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Camera open failed!")
+    sys.exit(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    data = cv2.imencode(".webp", frame, [cv2.IMWRITE_WEBP_QUALITY, 100]).tostring()
+    sender.emit("data", {"frame": data})
